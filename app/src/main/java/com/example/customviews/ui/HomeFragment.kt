7a5169af993a4bad509c255b2147d7f44d2593d0
mariamikv <mariamikv71@gmail.com>
2021-12-23 18:27:13 +0400

    update

--- app/src/main/java/com/example/customviews/ui/HomeFragment.kt
+++ app/src/main/java/com/example/customviews/ui/HomeFragment.kt
@@ -2,10 +2,8 @@ package com.example.customviews.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.customviews.databinding.FragmentHomeBinding
 import com.example.customviews.utils.BaseFragment
-import com.example.customviews.utils.Constants
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
     override fun startCreating(inflater: LayoutInflater, container: ViewGroup?) {
@@ -14,12 +12,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
 
     private fun init(){
         with(binding){
-            happyButton.setOnClickListener {
-                emotionalFaceView.happinessState = Constants.HAPPY
-            }
-            sadButton.setOnClickListener {
-                emotionalFaceView.happinessState = Constants.SAD
-            }
+
         }
     }
 }
